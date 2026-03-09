f81c77860aaa669e95f31ba39fee2acdbe3dc6e7
202407021KJB <jongbum1111@yuhan.ac.kr>
2026-03-10 08:43:57 +0900

    0309실습

---
+++ Project0309/Project0309/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project0309
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("유한대학교");
+            Console.WriteLine(3.14159265358979);
+
+            int salary = 2000000000;
+            Console.WriteLine("내 연봉은 " + salary + "원 ");
+        }
+    }
+}
