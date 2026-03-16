bce347e1ff900fa7aa4f7ef3b33c2c6c2f63ca84
KJB <jongbum1114@gmail.com>
2026-03-16 15:44:30 +0900

    0316실습

---
+++ Project0316/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project0316
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHP = 100;
+            int playerAttack = 10;
+
+            Console.WriteLine("플레이어 등장");
+            Console.WriteLine("HP : " + playerHP);
+            Console.WriteLine("Attack : " + playerAttack);
+            Console.WriteLine();
+
+            int monsterHP = 100;
+            int monsterAttack = 5;
+
+            Console.WriteLine("몬스터 등장");
+            Console.WriteLine("HP : " + monsterHP);
+            Console.WriteLine("Attack : " + monsterAttack);
+            Console.WriteLine();
+
+            Console.WriteLine("---------- 전투 시작 ----------");
+
+            while (playerHP > 0 && monsterHP > 0)
+            {
+                playerHP -= monsterAttack;
+                Console.WriteLine("몬스터의 공격");
+                Console.WriteLine("플레이어 HP : " + playerHP);
+
+                monsterHP -= playerAttack;
+                Console.WriteLine("플레이어의 공격");
+                Console.WriteLine("몬스터 HP : " + monsterHP);
+            }
+
+            Console.WriteLine("---------- 전투 종료 ----------");
+
+            int exp = 0;
+            int level = 1;
+            
+            if (playerHP <= 0) 
+            {
+                Console.WriteLine("몬스터 승리"); 
+            }
+            else if (monsterHP <= 0) 
+            {
+                Console.WriteLine("플레이어 승리");
+                exp += 10;
+            }
+
+            if (exp >= 10)
+            {
+                level++;
+                Console.WriteLine("레벨이 " + level + "로 상승");
+            }
+        }
+    }
+}
---
+++ Project0316_2/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Project0316_2
+{
+    /// 플레이어 설정
+    class Player
+    {
+        public int HP;
+        public int Attack;
+        public int Level;
+        public int Exp;
+
+        public Player()
+        {
+            HP = 100;
+            Attack = 10;
+            Level = 1;
+            Exp = 0;
+        }
+
+        public void AttackMonster(Monster monster)
+        {
+            monster.HP -= this.Attack;
+
+            Console.WriteLine("플레이어가 몬스터 공격");
+            Console.WriteLine("몬스터의 현재 HP : " + monster.HP);
+        }
+
+        public void GainExp(int amount)
+        {
+            Exp += amount;
+            if (Exp >= 10)
+            {
+                Level++;
+                Exp = 0;
+                HP = 100;
+                Attack += 5;
+
+                Console.WriteLine();
+                Console.WriteLine("레벨 업");
+                Console.WriteLine("레벨이 " + Level + "로 상승");
+            }
+        }
+
+        public void showState()
+        {
+            Console.WriteLine("플레이어의 HP : " + this.HP);
+            Console.WriteLine("플레이어의 Attack : " + this.Attack);
+            Console.WriteLine("플레이어의 Level : " + this.Level);
+            Console.WriteLine("플레이어의 Exp : " + this.Exp);
+        }
+    };
+
+    /// 몬스터 설정
+    class Monster
+    {
+        public int HP;
+        public int Attack;
+        public Monster()
+        {
+            HP = 50;
+            Attack = 5;
+        }
+
+        public void AttackPlayer(Player player)
+        {
+            player.HP -= this.Attack;
+
+            Console.WriteLine("몬스터가 플레이어 공격");
+            Console.WriteLine("플레이어의 현재 HP : " + player.HP);
+        }
+
+        public void showState()
+        {
+            Console.WriteLine("몬스터 HP : " + this.HP);
+            Console.WriteLine("몬스터 Attack : " + this.Attack);
+        }
+    };
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Player superMan = new Player();
+            Monster zombie = new Monster();
+            Console.WriteLine("---------- 전투 개시 ----------");
+            
+            while(superMan.HP > 0 && zombie.HP > 0)
+            {
+                Console.WriteLine("1. 공격");
+                Console.WriteLine("2. 도망");
+                Console.Write("선택 : ");
+
+                int input = 0;
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    superMan.AttackMonster(zombie);
+
+                    if (zombie.HP <= 0)
+                    {
+                        Console.WriteLine("플레이어 승리");
+                        superMan.GainExp(10);
+                        break;
+                    }
+
+                    zombie.AttackPlayer(superMan);
+                }
+                else if (input == 2)
+                {
+                    Console.WriteLine("도망간다");
+                    break;
+                }
+            }
+
+            Console.WriteLine("---------- 전투 종료 ----------");
+
+            /// 결과창 클래스 호출
+            superMan.showState();
+            Console.WriteLine();
+            zombie.showState();
+
+            //Console.WriteLine("플레이어의 HP : " + superMan.HP);
+            //Console.WriteLine("플레이어의 Attack : " + superMan.Attack);
+            //Console.WriteLine("플레이어의 Level : " + superMan.Level);
+            //Console.WriteLine("플레이어의 Exp : " + superMan.Exp);
+            //Console.WriteLine();
+
+            //Console.WriteLine("몬스터 HP : " + zombie.HP);
+            //Console.WriteLine("몬스터 Attack : " + zombie.Attack);
+        }
+    }
+}
