ab7cae2196a012e4afa00ea4fdd932434f1c9626
KJB <jongbum1114@gmail.com>
2026-04-20 15:49:55 +0900

    0420실습

---
+++ Project0420/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yuhan.ComSoft.CSharp;
+
+namespace Project0420
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /// IWeapon을 이용한 공격 방식 로직
+            //Character johnWick = new Character();
+            //Sword mugi_1 = new Sword("롱소드");
+            //johnWick.weapon = mugi_1;
+            //johnWick.AttackGo(1);
+            //johnWick.AttackGo(2);
+            //johnWick.AttackGo(2);
+
+            //Gun mugi_2 = new Gun("권총");
+            //johnWick.AttackGo(1);
+            //johnWick.AttackGo(2);
+            //johnWick.AttackGo(2);
+
+            /// IPayment를 이용한 결제 방식 로직
+            //Console.Write("결제 방식을 선택하세요(exit 입력 시 종료 : ");
+            //string input = Console.ReadLine();
+            //IPayment pay;
+            //switch (input)
+            //{
+            //    case "1":
+            //        pay = new CardPayment();
+            //        Console.WriteLine("카드 결제 방식 선택");
+            //        break;
+            //    case "2":
+            //        pay = new CashPayment();
+            //        Console.WriteLine("현금 결제 방식 선택");
+            //        break;
+            //    case "3":
+            //        pay = new KakaoPayment();
+            //        Console.WriteLine("카카오 페이 선택");
+            //        break;
+            //    case "exit":
+            //        Console.WriteLine("프로그램 종료");
+            //        return;
+            //    default:
+            //        Console.WriteLine("없는 결제 방식입니다.");
+            //        return;
+            //}
+            //pay.Pay(5000);
+
+            /// IPower, IInternet을 이용한 tv 동작 로직
+            //SmartTV tv = new SmartTV("삼성", 75);
+            //tv.On();
+            //Console.WriteLine("우리 집 TV 크기 : " + tv.Size + "인치");
+            //tv.Connect();
+            //tv.Off();
+
+            //RiceCooker bobsot = new RiceCooker("쿠첸", 5);
+            //bobsot.On();
+            //if (bobsot.Size >= 10)
+            //{
+            //    Console.WriteLine("삼계탕 요리 가능");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("삼계탕 요리 불가능");
+            //}
+            //Console.WriteLine("우리 집 밥솥은 " + bobsot.Size + "인용 입니다.");
+            //bobsot.Connect();
+            //bobsot.Off();
+
+
+            /// IPower, IInternet을 이용한 전자레인지 동작 로직
+            Microwave mw = new Microwave("삼성", "계란찜");
+            mw.On();
+            mw.Connect();
+            mw.Off();
+            
+        }
+    }
+}
---
+++ Project0420/yuhan.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuhan.ComSoft.CSharp
+{
+    //interface IWeapon
+    //{
+    //    void Attack(string w);
+    //}
+
+    //class Sword : IWeapon
+    //{
+    //    public string Name { get; set; }
+    //    public Sword(string sName)
+    //    {
+    //        Name = sName;
+    //    }
+
+    //    public void Attack(string w)
+    //    {
+    //        Console.WriteLine(Name + "로 " + w + " 공격");
+    //    }
+    //}
+
+    //class Gun : IWeapon
+    //{
+    //    public string Name { get; set; }
+    //    public Gun(string gName)
+    //    {
+    //        Name = gName;
+    //    }
+
+    //    public void Attack(string w)
+    //    {
+    //        Console.WriteLine(Name + "으로 공격");
+    //    }
+    //}
+
+    //class Character
+    //{
+    //    public IWeapon weapon;
+    //    public void AttackGo(int hand)
+    //    {
+    //        if (hand == 1) { weapon.Attack("양손"); }
+    //        else if (hand == 2) { weapon.Attack("한손"); }
+    //    }
+    //}
+
+    //interface IPayment
+    //{
+    //    void Pay(int amount);
+    //}
+
+    //class CardPayment : IPayment
+    //{
+    //    public void Pay(int amount)
+    //    {
+    //        Console.WriteLine("카드 : " + amount + "원 결제");
+    //    }
+    //}
+
+    //class CashPayment : IPayment
+    //{
+    //    public void Pay(int amount)
+    //    {
+    //        Console.WriteLine("현금 : " + amount + "원 결제");
+    //    }
+    //}
+
+    //class KakaoPayment : IPayment
+    //{
+    //    public void Pay(int amount)
+    //    {
+    //        Console.WriteLine("카카오 페이 : " + amount + "원 결제");
+    //    }
+    //}
+
+    interface IPower
+    {
+        void On();
+        void Off();
+    }
+
+    interface  IInternet
+    {
+        void Connect();
+    }
+
+    class SmartTV : IPower, IInternet
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+
+        public SmartTV(string brand, int inch)
+        {
+            Name = brand;
+            Size = inch;
+        }
+
+        public void On()
+        {
+            Console.WriteLine(Name + "TV 전원 ON");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine(Name + "TV 전원 OFF");
+        }
+
+        public void Connect()
+        {
+            Console.WriteLine(Name + "TV 인터넷 연결 성공");
+        }
+    }
+
+    class RiceCooker : IPower, IInternet
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+
+        public RiceCooker(string brand, int total)
+        {
+            Name = brand;
+            Size = total;
+        }
+        public void On()
+        {
+            Console.WriteLine(Name + "(이)가 맛있는 요리를 시작했습니다.");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine(Name + "(이)가 맛있는 요리를 완성하였습니다.\n보온모드로 전환됩니다.");
+        }
+
+        public void Connect()
+        {
+            Console.WriteLine("WIFI로 인터넷 연결");
+        }
+    }
+
+    class Microwave : IPower, IInternet
+    {
+        public string Name { get; set; }
+        public string Food { get; set; }
+
+        public Microwave(string brand, string cook)
+        {
+            Name = brand;
+            Food = cook;
+        }
+
+        public void On()
+        {
+            Console.WriteLine($"{Name} 전자레인지가 {Food}을(를) 만듭니다.");
+        }
+        
+        public void Off()
+        {
+            Console.WriteLine($"{Name} 전자레인지가 {Food}을(를) 완성했습니다.\n전원이 꺼집니다.");
+        }
+
+        public void Connect()
+        {
+            Console.WriteLine($"{Name} 전자레인지가 WIFI에 연결되었습니다.");
+        }
+    }
+    internal class yuhan
+    {
+    }
+}
